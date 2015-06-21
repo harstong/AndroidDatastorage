642ca6e2a203fd9be02e73ba1dfcbbb4ee718683
fieryshadow <jen13077@byui.edu>
2015-06-21 01:33:34 -0600

    make the app use a database instead of shared preferences

--- app/src/main/java/com/example/greg/androiddatastorage/MainActivity.java
+++ app/src/main/java/com/example/greg/androiddatastorage/MainActivity.java
@@ -1,27 +1,46 @@
 package com.example.greg.androiddatastorage;
 
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+    private CountDataSource dataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences settings = getPreferences(0);
+        int count = 0;
+        dataSource = new CountDataSource(this);
+        try {
+            dataSource.open();
+            List<Integer> counts = dataSource.getAllCounts();
+            if (counts.size() > 0) {
+                count = counts.get(0);
+            }
+            dataSource.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         TextView t = (TextView) findViewById(R.id.Count);
-        t.setText(Integer.toString(settings.getInt("count", 0)));
+        t.setText(Integer.toString(count));
     }
 
     @Override
@@ -57,30 +76,107 @@ public class MainActivity extends ActionBarActivity {
         TextView t = (TextView) findViewById(R.id.Count);
         int value = Integer.parseInt(String.valueOf(t.getText()));
 
-        SharedPreferences.Editor settings = getPreferences(0).edit();
-
-        settings.putInt("count", value);
-        settings.commit();
+        try {
+            dataSource.open();
+            dataSource.replaceCount(0, value);
+            dataSource.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
 }
 
-class DataBase extends SQLiteOpenHelper {
-    private static final String NAME = "DataBase";
-    private static final int VERSION = 2;
+class CountDataSource {
+    private SQLiteDatabase database;
+    private MyDatabaseHelper dbHelper;
+    private String[] allColumns = { MyDatabaseHelper.COLUMN_ID, MyDatabaseHelper.COLUMN_COUNT };
 
-    public DataBase(Context context) {
-        super(context, NAME, null, VERSION);
-        
+    public CountDataSource(Context context) {
+        dbHelper = new MyDatabaseHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long storeCount(int count) {
+        ContentValues values = new ContentValues();
+        values.put(MyDatabaseHelper.COLUMN_COUNT, count);
+        return database.insert(MyDatabaseHelper.TABLE_COUNTS, null, values);
+    }
+
+    public void deleteCount(long rowId) {
+        database.delete(MyDatabaseHelper.TABLE_COUNTS,
+                MyDatabaseHelper.COLUMN_ID + " = " + rowId, null);
+    }
+
+    public void replaceCount(long rowId, int value) {
+        ContentValues values = new ContentValues();
+        values.put(MyDatabaseHelper.COLUMN_ID, 0);
+        values.put(MyDatabaseHelper.COLUMN_COUNT, value);
+        database.replace(MyDatabaseHelper.TABLE_COUNTS, null, values);
+    }
+
+    public int getCount(int rowId) {
+        Cursor cursor = database.query(MyDatabaseHelper.TABLE_COUNTS, allColumns,
+                MyDatabaseHelper.COLUMN_ID + " = " + rowId, null, null, null, null);
+        cursor.moveToFirst();
+        int count = cursor2count(cursor);
+        cursor.close();
+        return count;
+    }
+
+    public List<Integer> getAllCounts() {
+        List<Integer> counts = new ArrayList<>();
+        Cursor cursor = database.query(MyDatabaseHelper.TABLE_COUNTS,
+                allColumns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            counts.add(cursor.getInt(1));
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        return counts;
+    }
+
+    private int cursor2count(Cursor cursor) {
+        return cursor.getInt(1);
+    }
+}
+
+class MyDatabaseHelper extends SQLiteOpenHelper {
+    public static final String TABLE_COUNTS = "counts";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_COUNT = "count";
+
+    public static final String DATABASE_NAME = "count.db";
+    public static final int VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table " + TABLE_COUNTS + "(" +
+            COLUMN_ID + " integer primary key autoincrement, " +
+            COLUMN_COUNT + " text not null);";
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(DATABASE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.w(MyDatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", destroying all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COUNTS);
+        onCreate(db);
     }
 }
